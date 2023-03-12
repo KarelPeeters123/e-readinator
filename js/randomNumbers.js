55304fab9bb5d038329d02dc7f743a8b2f963b28
Egg-0246 <107084007+Egg-0246@users.noreply.github.com>
2023-03-12 13:45:09 +1300

    update RandomNumbers
    Added a function that chooses a random number between 4 and 20 for font size.

--- js/randomNumbers.js
+++ js/randomNumbers.js
@@ -8,10 +8,19 @@ function threeRandomNumbers () {
     console.log(array);
 }
 
+<<<<<<< Updated upstream
 function randomNumberBetween4And20 () {
     let randomFontSize = Math.floor(Math.random() * 17) + 4;
     console.log(randomFontSize);
 }
 
 let x = randomNumberBetween4And20();
+=======
+function randomBetween4And20 () {
+    let fontSizeRandom = Math.floor(Math.random() * 17) + 4;
+    console.log (fontSizeRandom);
+}
+
+let x = randomBetween4And20();
+>>>>>>> Stashed changes
 console.log(x);
